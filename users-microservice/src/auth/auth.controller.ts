7a2f9981732b2feb8733dd824cba51640ff6ab98
david <dm7659746@gmail.com>
2025-06-28 18:20:29 -0500

    feat: create endpoint login

---
+++ users-microservice/src/auth/auth.controller.ts
@@ -0,0 +1,14 @@
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller()
+export class AuthMicroserviceController {
+  constructor(private authService: AuthService) {}
+
+  @MessagePattern({ cmd: 'auth_login' })
+  async handleLogin(@Payload() data: { email: string; password: string }) {
+    const { email, password } = data;
+    return this.authService.validateUser(email, password);
+  }
+}
---
+++ users-microservice/src/auth/auth.service.ts
@@ -0,0 +1,36 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.usersService.findByEmail(email);
+    if (!user) {
+      throw new UnauthorizedException('Credenciales inválidas');
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Credenciales inválidas');
+    }
+
+    const payload = { sub: user.id, email: user.email };
+    const token = this.jwtService.sign(payload);
+
+    return {
+      accessToken: token,
+      user: {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+      },
+    };
+  }
+}
