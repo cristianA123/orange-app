8e893f841c7593e093e92bf5ba7c126e286d881d
dev-cristian <cristian.chipana@kambista.com>
2025-07-05 20:19:31 -0500

    feat: add institute module with controller and DTOs; remove unused RPC error handler

---
+++ http-api-gateway/src/modules/institute/institute.controller.ts
@@ -0,0 +1,69 @@
+import { Controller, HttpCode, HttpStatus, Inject, Post } from '@nestjs/common';
+import { ClientProxy, Payload } from '@nestjs/microservices';
+import { CreateInstituteDto } from './dtos/create-institute.dto';
+// import { UpdateInstituteDto } from './dtos/update-institute.dto';
+import { handleRpcError } from 'src/common/erros/error-handler';
+import { lastValueFrom } from 'rxjs';
+
+@Controller('institute')
+export class InstituteController {
+  constructor(@Inject('NATS_SERVICE') private natsClient: ClientProxy) {}
+
+  // @Post()
+  // @HttpCode(HttpStatus.OK)
+  // async createUser(@Body() createUserDto: CreateUserDto) {
+  //   console.log(createUserDto);
+
+  //   try {
+  //     const response = await lastValueFrom(
+  //       this.natsClient.send({ cmd: 'createUser' }, createUserDto),
+  //     );
+
+  //     return response;
+  //   } catch (error) {
+  //     console.log('perrito2');
+  //     console.error(error);
+  //     handleRpcError(error);
+  //   }
+  // }
+
+  // @MessagePattern({ cmd: 'getUserById' })
+  @Post('/')
+  @HttpCode(HttpStatus.OK)
+  async create(@Payload() createInstituteDto: CreateInstituteDto) {
+    try {
+      // return { hola: 1, createInstituteDto };
+      const response = await lastValueFrom(
+        this.natsClient.send({ cmd: 'CREATE_INSTITUTE' }, createInstituteDto),
+      );
+
+      return response;
+    } catch (error) {
+      console.error(error);
+      handleRpcError(error);
+    }
+  }
+
+  // @MessagePattern('findAllInstitute')
+  // findAll() {
+  //   return this.instituteService.findAll();
+  // }
+
+  // @MessagePattern('findOneInstitute')
+  // findOne(@Payload() id: number) {
+  //   return this.instituteService.findOne(id);
+  // }
+
+  // @MessagePattern('updateInstitute')
+  // update(@Payload() updateInstituteDto: UpdateInstituteDto) {
+  //   return this.instituteService.update(
+  //     updateInstituteDto.id,
+  //     updateInstituteDto,
+  //   );
+  // }
+
+  // @MessagePattern('removeInstitute')
+  // remove(@Payload() id: number) {
+  //   return this.instituteService.remove(id);
+  // }
+}
