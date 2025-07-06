e5bd2a8c4aed2cf10c554e799d77024a60d5ef43
dev-cristian <cristian.chipana@kambista.com>
2025-07-05 20:19:31 -0500

    feat: add institute module with controller and DTOs; remove unused RPC error handler

--- http-api-gateway/src/app.module.ts
+++ http-api-gateway/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { PaymentsModule } from './payments/payments.module';
 import { AuthModule } from './auth/auth.module';
+import { InstituteModule } from './modules/institute/institute.module';
 
 @Module({
-  imports: [UsersModule, PaymentsModule, AuthModule],
+  imports: [UsersModule, PaymentsModule, AuthModule, InstituteModule],
   controllers: [],
   providers: [],
 })
