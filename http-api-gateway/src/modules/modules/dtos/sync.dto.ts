d98c907317f2e1064615f618892800b7da69d862
david <dm7659746@gmail.com>
2025-07-23 17:05:28 -0500

    feat: add endpoint sync modules institute

--- http-api-gateway/src/modules/modules/dtos/sync.dto.ts
+++ http-api-gateway/src/modules/modules/dtos/sync.dto.ts
@@ -1,8 +1,18 @@
 import { IsArray, ArrayNotEmpty, IsUUID } from 'class-validator';
 
-export class SyncModuleDto {
+export class SyncModuleUserDto {
   @IsUUID()
   userId: string;
+
+  @IsArray()
+  @ArrayNotEmpty()
+  @IsUUID('all', { each: true })
+  modulesIds: string[];
+}
+
+export class SyncModuleInstituteDto {
+  @IsUUID()
+  instituteId: string;
 
   @IsArray()
   @ArrayNotEmpty()
--- modules-microservice/src/modules/module.module.ts
+++ modules-microservice/src/modules/module.module.ts
@@ -4,9 +4,12 @@ import { ModuleMicroserviceController } from './module.controller';
 import { ModuleService } from './module.service';
 import { UserModule } from '../typeorm/entities/UserModule';
 import { Module as ModuleModel } from '../typeorm/entities/Module';
+import { InstituteModule } from '../typeorm/entities/InstituteModule';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserModule, ModuleModel])],
+  imports: [
+    TypeOrmModule.forFeature([UserModule, ModuleModel, InstituteModule]),
+  ],
   controllers: [ModuleMicroserviceController],
   providers: [ModuleService],
 })
