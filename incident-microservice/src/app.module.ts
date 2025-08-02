d4eaea0124d3f9df1db27afda7aa50835ad0af17
dev-cristian <cristian.chipana@kambista.com>
2025-08-01 23:42:28 -0500

    feat: initialize incident microservice with core functionality
    - Set up basic NestJS microservice structure with NATS transport
    - Implemented incident and institute modules with CRUD operations
    - Added TypeORM entities for incident management system
    - Configured database connection with MySQL
    - Included essential configuration files (Docker, ESLint, Prettier)
    - Added testing setup with Jest for unit and e2e tests

---
+++ incident-microservice/src/app.module.ts
@@ -0,0 +1,37 @@
+import { databaseConfig } from './data-source';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './typeorm/entities/User';
+import { Institute } from './typeorm/entities/Institute';
+import { InstituteModule } from './typeorm/entities/InstituteModule';
+import { UserModule } from './typeorm/entities/UserModule';
+import { Incident } from './typeorm/entities/Incident';
+import { IncidentFile } from './typeorm/entities/IncidentFile';
+import { Module as ModuleTypeOrm } from './typeorm/entities/Module';
+import { InstituteModule as InstituteModule2 } from './institute/institute.module';
+import { IncidentModule } from './incident/incident.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      ...databaseConfig,
+      host: 'mysql_db',
+      entities: [
+        User,
+        Institute,
+        ModuleTypeOrm,
+        InstituteModule,
+        UserModule,
+        Incident,
+        IncidentFile,
+      ],
+      synchronize: false,
+      migrations: [__dirname + '/migrations/*.ts'],
+    }),
+    InstituteModule2,
+    IncidentModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ incident-microservice/src/institute/dto/update-institute.dto.ts
@@ -0,0 +1,6 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateInstituteDto } from './create-institute.dto';
+
+export class UpdateInstituteDto extends PartialType(CreateInstituteDto) {
+  id: number;
+}
