b79ccc57b84af79ad63b65b46e27ff42da058dcb
david <dm7659746@gmail.com>
2025-09-02 23:03:34 -0500

    fix: connect dashboard to db

--- dashboard-microservice/src/app.module.ts
+++ dashboard-microservice/src/app.module.ts
@@ -8,26 +8,32 @@ import { Incident } from './typeorm/entities/Incident';
 import { IncidentFile } from './typeorm/entities/IncidentFile';
 import { Module as ModuleTypeOrm } from './typeorm/entities/Module';
 import { DashboardModule } from './dashboard/dashboard.module';
+import configuration from "./confi/configuration";
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      ...databaseConfig,
-      host: 'mysql_db',
-      entities: [
-        User,
-        Institute,
-        ModuleTypeOrm,
-        InstituteModule,
-        UserModule,
-        Incident,
-        IncidentFile,
-      ],
-      synchronize: false,
-      migrations: [__dirname + '/migrations/*.ts'],
-    }),
-      DashboardModule,
-  ],
-  controllers: [],
-  providers: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            load: [configuration],
+        }),
+        TypeOrmModule.forRoot({
+            ...databaseConfig,
+            entities: [
+                User,
+                Institute,
+                ModuleTypeOrm,
+                InstituteModule,
+                UserModule,
+                Incident,
+                IncidentFile,
+            ],
+            synchronize: false,
+            migrations: [__dirname + '/migrations/*.ts'],
+        }),
+        DashboardModule,
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
